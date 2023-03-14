c8c1bdc1c8c772f03fa4a726ea63faf0e0a7c3c8
kashif tufail <kashif.tufail17@gmail.com>
2023-03-14 10:53:35 +0500

    refactor code and add new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'pages/contact'
   get 'home/blog'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
