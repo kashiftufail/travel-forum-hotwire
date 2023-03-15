84817d1007c8c1dd4b2621643cef573be2242ab5
kashif tufail <kashif.tufail17@gmail.com>
2023-03-15 18:52:56 +0500

    add member type to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_143202) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_135024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_143202) do
     t.string "instagram_uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "member_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
