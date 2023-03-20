f54a0c41a7eddf1810e0f36a7b010ea5eaa717f2
kashif tufail <kashif.tufail17@gmail.com>
2023-03-20 21:17:03 +0500

    add partial and devise configutaion for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def collapsible_welcome_path
+    if user_signed_in?
+      render partial: 'shared/sign_in_nav'
+    else  
+      link_to "Login" , new_user_session_path, class: "nav-item nav-link" 
+    end      
+  end      
 end
