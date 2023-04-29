2582aeeb48a857969d2330eac3eb5fde7c5c5588
kashif tufail <kashif.tufail17@gmail.com>
2023-04-29 13:08:22 +0500

    add flatpicker for date selection

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,13 @@ import "./controllers";
 // import * as bootstrap from "bootstrap"
 import "@fortawesome/fontawesome-free/js/all";
 import "./src/jquery";
+import './src/jquery-easy-autocomplete-min'
 import "./src/bootstrap-min";
+import flatpickr from "flatpickr";
+import './src/flatpickr';
 import "./src/owl-carousel";
 import './src/main'
+
+
+
 
