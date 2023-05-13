0de17824532ebb144f4f2f3fb0338cc10060d310
kashif tufail <kashif.tufail17@gmail.com>
2023-05-13 23:41:16 +0500

    action mailer configuration for sent mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'kashif.tufail17@gmail.com'}
+  config.action_mailer.default_url_options = { host: 'https://glassinfotech.com/' }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'glassinfotech.com',
+    user_name:             Rails.application.credentials.dig(:gmail, :name),
+    password:              Rails.application.credentials.dig(:gmail, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+
 end
 
