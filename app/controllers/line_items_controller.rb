56b215e79e077cb4954c3c601a61ab05d098237c
kashif tufail <kashif.tufail17@gmail.com>
2023-07-06 14:45:50 +0500

    add product to cart functoinality using turbo stream

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
 
-  def create
+  def create  
     # Find associated product and current cart
     chosen_product = Product.find(params[:product_id])
     current_cart = @current_cart
@@ -8,18 +8,24 @@ class LineItemsController < ApplicationController
     # If cart already has this product then find the relevant line_item and iterate quantity otherwise create a new line_item for this product
     if current_cart.products.include?(chosen_product)
       # Find the line_item with the chosen_product
-      @line_item = current_cart.line_items.find_by(:product_id => chosen_product)
+      @line_item = current_cart.line_items.find_by(product_id: chosen_product)
       # Iterate the line_item's quantity by one
       @line_item.quantity += 1
     else
       @line_item = LineItem.new
       @line_item.cart = current_cart
       @line_item.product = chosen_product
+      @line_item.quantity = 1      
     end
   
     # Save and redirect to cart show path
-    @line_item.save
-    redirect_to cart_path(current_cart)
+    respond_to do |format|            
+      if @line_item.save        
+        format.turbo_stream
+      else        
+        format.html { redirect_to products_path, alert: @line_item.errors.full_messages }
+      end
+    end  
   end
   
   private
